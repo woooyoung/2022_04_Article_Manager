0be6d7d6c041e778370ca597c0a52061d7ac7855
woooyoung <axdsw121@gmail.com>
2022-05-18 10:01:49 +0900

    스캐너 연습

---
+++ src/com/KoreaIT/java/AM/Main.java
@@ -0,0 +1,22 @@
+package com.KoreaIT.java.AM;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("==프로그램 시작==");
+
+		Scanner sc = new Scanner(System.in);
+		System.out.printf("명령어 ) ");
+		String command;
+		command = sc.next();
+		System.out.printf("입력된 명령어 : %s\n", command);
+
+		int num = sc.nextInt();
+		System.out.printf("입력된 정수 : %d\n", num);
+
+		sc.close();
+
+		System.out.println("==프로그램 끝==");
+	}
+}
