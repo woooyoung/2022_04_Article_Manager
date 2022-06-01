503abc9414d339bd488f4af3e3604eb47fbf1543
woooyoung <axdsw121@gmail.com>
2022-06-01 10:43:04 +0900

    프로그램 시작시 회원 테스트데이터 생성

--- src/com/KoreaIT/java/AM/controller/MemberController.java
+++ src/com/KoreaIT/java/AM/controller/MemberController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import com.KoreaIT.java.AM.dto.Article;
 import com.KoreaIT.java.AM.dto.Member;
 import com.KoreaIT.java.AM.util.Util;
 
@@ -58,6 +59,14 @@ public class MemberController extends Controller {
 
 		loginedMember = member;
 		System.out.printf("로그인 성공!, %s님 환영합니다.\n", loginedMember.name);
+	}
+
+	public void makeTestData() {
+		System.out.println("테스트를 위한 회원 데이터를 생성합니다.");
+
+		members.add(new Member(1, Util.getNowDateStr(), "admin", "admin", "관리자"));
+		members.add(new Member(2, Util.getNowDateStr(), "test1", "test1", "유저1"));
+		members.add(new Member(3, Util.getNowDateStr(), "test2", "test2", "유저2"));
 	}
 
 	private Member getMemberByLoginId(String loginId) {
