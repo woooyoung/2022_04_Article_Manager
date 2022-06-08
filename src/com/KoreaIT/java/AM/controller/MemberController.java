fe215a888d9dbd8201d254786dc4a1bb932a5c3d
woooyoung <axdsw121@gmail.com>
2022-06-08 11:08:09 +0900

    한번 사용된 게시물의 번호가 재사용 되지 않도록 처리

--- src/com/KoreaIT/java/AM/controller/MemberController.java
+++ src/com/KoreaIT/java/AM/controller/MemberController.java
@@ -72,9 +72,12 @@ public class MemberController extends Controller {
 	public void makeTestData() {
 		System.out.println("테스트를 위한 회원 데이터를 생성합니다.");
 
-		members.add(new Member(1, Util.getNowDateStr(), "admin", "admin", "관리자"));
-		members.add(new Member(2, Util.getNowDateStr(), "test1", "test1", "김철수"));
-		members.add(new Member(3, Util.getNowDateStr(), "test2", "test2", "김영희"));
+		Container.memberDao
+				.add(new Member(Container.memberDao.getNewId(), Util.getNowDateStr(), "admin", "admin", "관리자"));
+		Container.memberDao
+				.add(new Member(Container.memberDao.getNewId(), Util.getNowDateStr(), "test1", "test1", "김철수"));
+		Container.memberDao
+				.add(new Member(Container.memberDao.getNewId(), Util.getNowDateStr(), "test2", "test2", "김영희"));
 	}
 
 	private Member getMemberByLoginId(String loginId) {
@@ -88,7 +91,7 @@ public class MemberController extends Controller {
 	}
 
 	private void doJoin() {
-		int id = members.size() + 1;
+		int id = Container.memberDao.getNewId();
 		String regDate = Util.getNowDateStr();
 
 		String loginId = null;
@@ -126,7 +129,7 @@ public class MemberController extends Controller {
 		String name = sc.nextLine();
 
 		Member member = new Member(id, regDate, loginId, loginPw, name);
-		members.add(member);
+		Container.memberDao.add(member);
 
 		System.out.printf("%d번 회원이 가입하였습니다\n", id);
 
---
+++ src/com/KoreaIT/java/AM/dao/Dao.java
@@ -0,0 +1,18 @@
+package com.KoreaIT.java.AM.dao;
+
+public abstract class Dao {
+
+	protected int lastId;
+
+	Dao() {
+		lastId = 0;
+	}
+
+	public int getLastId() {
+		return lastId;
+	}
+
+	public int getNewId() {
+		return lastId + 1;
+	}
+}
